1118c858042cee55350020d40fbfc42d57454f77
r-ush <aarush.bhatt@gmail.com>
2021-03-21 03:30:50 +0530

    feat: pwa

--- src/pages/ob2.jsx
+++ src/pages/ob2.jsx
@@ -50,7 +50,7 @@ const OB2 = () => {
                     alignSelf: "baseline",
                     marginTop: "50px",
                     position: "absolute",
-                    bottom: "200px",
+                    bottom: "150px",
                 }}
                 src={slider2}
                 alt=""
